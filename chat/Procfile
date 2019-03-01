32bebefcb969930378c26f179c22eb78034f2f4c
ChetanBM <chetanbanavikalmutt1997@gmail.com>
2019-03-01 12:46:22 +0530

    COnfigured to heroku

--- chat/Procfile
+++ chat/Procfile
@@ -1,1 +1,1 @@
-web: gunicorn chat.wsgi
+web: gunicorn chat.wsgi --log-file -
--- chat/settings.py
+++ chat/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -130,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/chat'
 LOGOUT_REDIRECT_URL = '/chat'
